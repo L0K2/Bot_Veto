f524c884e1481b253f346ac938d0722addb34882
Rafael Soster <70975828+L0K2@users.noreply.github.com>
2022-03-08 19:13:43 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+
+TOKEN = "OTM4OTA5NTQ4NTExNjI1MjY2.YfxJqA.rQLKlIumLqGC1qfdVi2D_MfK-ao"
+
+client = commands.Bot(command_prefix='!')
+
+mapas_save = ["Inferno", "Mirage", "Nuke", "Overpass", "Dust2", "Vertigo", "Ancient"]
+mapas = ["Inferno", "Mirage", "Nuke", "Overpass", "Dust2", "Vertigo", "Ancient"]
+
+@client.event
+async def on_ready():
+    print('Bot logado')
+
+
+@client.command()
+async def teste(ctx):
+    await ctx.send('O bot foi testado')
+
+@client.command()
+async def veto(ctx):
+    mapas = mapas_save
+    await ctx.send('O map pool é: Inferno, Mirage, Nuke, Overpass, Dust2, Vertigo, Ancient')
+    await ctx.send('Qual mapa remover(!ban + Nome do mapa): ')
+
+
+
+@client.command()
+async def ban(ctx,arg):
+        num_de_ban = 0
+        if num_de_ban == 0:
+            mapas.remove(arg)
+            ban1 = mapas
+            num_de_ban = num_de_ban + 1
+            await ctx.send("Os mapas restantes são: ")
+            await ctx.send(ban1)
+            await ctx.send("Qual mapa remover:")
+        if len(mapas) == 1:
+            num_de_ban = 2
+            await ctx.send("O mapa jogado será: ", mapas)
+
+
+
+
+
+
+
+
+client.run(TOKEN)
