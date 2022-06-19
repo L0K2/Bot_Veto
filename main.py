d4712ede0ba2a9ecf0c3a1a4ba201f8e80efae88
Rafael Soster <70975828+L0K2@users.noreply.github.com>
2022-06-19 15:21:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ TOKEN = ""
 client = commands.Bot(command_prefix='!')
 
 mapas_save = ["Inferno", "Mirage", "Nuke", "Overpass", "Dust2", "Vertigo", "Ancient"]
-mapas = ["Inferno", "Mirage", "Nuke", "Overpass", "Dust2", "Vertigo", "Ancient"]
+mapas = []
 
 @client.event
 async def on_ready():
@@ -19,7 +19,7 @@ async def teste(ctx):
 
 @client.command()
 async def veto(ctx):
-    mapas = mapas_save #problema: mapas n definido
+    main.mapas = main.mapas_save.copy()
     await ctx.send('O map pool é: Inferno, Mirage, Nuke, Overpass, Dust2, Vertigo, Ancient')
     await ctx.send('Qual mapa remover(!ban + Nome do mapa): ')
 
@@ -30,13 +30,13 @@ async def veto(ctx):
 async def ban(ctx,arg):
     if ctx.author == client.user:
         return
-    while len(mapas) > 1:
+        while len(mapas) > 1:
             mapas.remove(arg)
             await ctx.send(f"{ctx.author.mention} Removeu {arg}")
             await ctx.send("Os mapas restantes são: ")
             await ctx.send(mapas)
-            await ctx.send("Qual mapa remover:")
-            await ctx.send(len(mapas))
+            if len(mapas) != 1:
+                await ctx.send("Qual mapa remover:")
     if len(mapas) == 1:
             await ctx.send(f"O mapa jogado será:  {mapas}")
 
